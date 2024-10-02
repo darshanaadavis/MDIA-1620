6d446dfeee0eadaa4207e29a73b8e6a95149a745
darshanaadavis <darshanaa_davis@outlook.com>
2024-10-02 09:33:07 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -1,7 +1,12 @@
-let horseName = "Bear"
-console.log(horseName);
 
-let horseAge = 5;
-console.log(horseAge);
-let isHorseInside = true;
-console.log(isHorseInside)
+let horseName = "Tabby"
+console.log ("Hello, this is my horse Tabitha. But most of the time we just call her " + horseName);
+
+constSTABLE_MONTHLY_FEE = 130
+console.log("The monthly fee for the stables is " + constSTABLE_MONTHLY_FEE);
+console.log("If you pay 3 months up front, you will get 10% off and save " + constSTABLE_MONTHLY_FEE*3*0.1 +"$");
+
+
+
+//*Console.log out a message that introduces your horse and shares their nickname. Use a variable to store the nickname*//
+//*Console.log out a message that states how much you would save with a 10% discount applied to a 3 month stay at your stables.*//
